9aecf774cc1483b383d89e87310c195d331846a9
Nhat Thien An <nhatthienan11@gmail.com>
2025-07-06 16:02:15 +0700

    Add en-core-web-sm in matcher.py

--- modules/matcher.py
+++ modules/matcher.py
@@ -1,7 +1,13 @@
 import spacy
 import yaml
+from spacy.cli import download
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 def load_weights():
     try:
